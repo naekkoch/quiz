787bc00c874359cb95286a78946916d5bb26d4df
naekkoch <micaelaas3105@gmail.com>
2023-10-25 21:54:05 -0300

    TERMINEI O QUIZgit add --all

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function showQuestion() {
   if (questions[currentQuestion]) {
     let q = questions[currentQuestion];
     let progressBarPercentage = Math.floor(
-      (currentQuestion / questions.lenght) * 100
+      (currentQuestion / questions.length) * 100
     );
-    document.querySelector(
-      ".progress--bar").style.width = `${progressBarPercentage}%`;
+    document.querySelector(".progress--bar"
+    ).style.width = `${progressBarPercentage}%`;
     document.querySelector(".scoreArea").style.display = "none";
     document.querySelector(".questionArea").style.display = "block";
     document.querySelector(".question").textContent = q.question;
@@ -24,7 +24,8 @@ function showQuestion() {
     let optionsHtml = "";
     for (let i = 0; i < q.options.length; i++) {
       optionsHtml += `<div data-op="${i}" class="option"><span>${
-        parseInt(i) + 1}</span>${q.options[i]}</div>`;
+        parseInt(i) + 1
+      }</span>${q.options[i]}</div>`;
     }
     document.querySelector(".options").innerHTML = optionsHtml;
     document.querySelectorAll(".options .option").forEach((item) => {
@@ -34,27 +35,45 @@ function showQuestion() {
     finishQuiz();
   }
 }
-
 function optionClickEvent(e) {
   let questaoClicada = parseInt(e.target.getAttribute("data-op"));
-  if (questions[currentQuestion].answer === questaoClicada);
-  correctAnswers++;
-}
-{
+  if (questions[currentQuestion].answer === questaoClicada) {
+    correctAnswers++;
+  }
+
   currentQuestion++;
   showQuestion();
 }
-
 function finishQuiz() {
-  // Criar variável de pontos baseado na divisão entre respostas corretas e quantidade de questões. Use a função Math.floor para arredondar.
-  // Implementar condicionais para inserir mensagem e cor do placar de acordo com a pontuação.
-  // Usar condicional if e condicionais <, <=, >, >=
-  // Inserir a pontuação em .scorePct e o texto em .scoreText2
-  // Ocultar a .questionArea e exibir a .scoreArea
-  // Deixar a .progress--bar em 100%
+  let score = Math.floor((correctAnswers / questions.length) * 100);
+  let scoreColor = "white";
+  let message = "";
+  if (score > 80 && score >= 90) {
+    scoreColor = "green";
+    message = "Perfeito!!";
+  } else if (score > 70 && score >= 80) {
+    scoreColor = "blue";
+    message = "Muito bem!";
+  } else if (score > 60 && score >= 70) {
+    scoreColor = "yellow";
+    message = "Foi bem";
+  } else if (score > 50 && score >= 60) {
+    scoreColor = "orange";
+    message = "Dá para melhorar hein";
+  } else {
+    scoreColor = "red";
+    message = "Opss...pelo menos tentou né";
+  }
+  document.querySelector(
+    ".scoreText2").textContent = `Você respondeu ${questions.length} questões e acertou ${correctAnswers}.`;
+  document.querySelector(".scorePct").textContent = `${score}%`;
+  document.querySelector(".scoreText1").textContent = message;
+  document.querySelector(".questionArea").style.display = "none";
+  document.querySelector(".scoreArea").style.display = "block";
+  document.querySelector(".progress--bar").style.width = `100%`;
 }
-
 function resetEvent() {
-  // Redefina os valores de correctAnswers e currentQuestion para 0
-  // Chame a função showQuestion
+  correctAnswers = 0;
+  currentQuestion = 0;
+  showQuestion();
 }
--- question.js
+++ question.js
@@ -5,4 +5,85 @@ let questions = [
     options: ["tag js", "tag script", "tag scripting", "tag javascript"],
     answer: 1,
   },
+  {
+    question: "Como que todo código de HTML começa?",
+    options: [
+      "&lt;html&gt;&lt;/html&gt;",
+      "&lthead&gt;&lt/head&gt;",
+      "&ltstart&gt;&lt/start&gt;",
+      "&lt!DOCTYPE html&gt;",
+    ],
+    answer: 3,
+  },
+  {
+    question: "Sobre CSS, é correto afirmar, EXCETO:",
+    options: [
+      "Cada declaração de propriedade termina com uma vírgula",
+      "As CSS contêm declarações simples, chamadas regras.",
+      "Todos os valores ficam entre chaves { }",
+      "Um elemento pode pertencer a mais de uma classe",
+    ],
+    answer: 0,
+  },
+  {
+    question: "Para que serve a tag <head></head>",
+    options: [
+      "Para nomear o site",
+      "Adicionar coisas na cabeça do site",
+      "Para escrever textos",
+      "Adicionar coisas no corpo do site",
+    ],
+    answer: 1,
+  },
+  {
+    question: "A instrução Var serve para:",
+    options: [
+      "Forçar o tipo de variável",
+      "Atribuir valor a uma variável",
+      "Indicar ao interpretador de JS que uma nova variável está sendo declarada",
+      "A break é capaz de fazer o loop retornar ao seu inicio",
+    ],
+    answer: 2,
+  },
+  {
+    question:
+      "Selecione a alternativa que representa todos os seletores básicos.",
+    options: [
+      "tag, classe, id, universais e atributo.",
+      "tag, classe, id e atributo.",
+      "classe, universais e tag.",
+      "tag, classe e id.",
+    ],
+    answer: 0,
+  },
+  {
+    question: "Selecione a alternativa que não representa uma pseudo-classe.",
+    options: ["disabled", "clicked", "hover", "active"],
+    answer: 1,
+  },
+  {
+    question: "O que significa a sigla CSS?",
+    options: [
+      "Constrution of Style Sites.",
+      "Cascading Style Sheets.",
+      "Consortium of Style Sheets.",
+      "Folhas de Estilo em Cascalhos",
+    ],
+    answer: 1,
+  },
+  {
+    question: " Qual a sintaxe para inserir um comentário CSS?",
+    options: [
+      "// Este é um comentário",
+      " /* Este é um comentário */",
+      "	&lt;!--Este é um comentário--&gt;",
+      "#Este é um comentário",
+    ],
+    answer: 2,
+  },
+  {
+    question: "Que propriedade CSS controla o tamanho do texto?",
+    options: ["text-height", "text-aling", " font-size", "text-size"],
+    answer: 3,
+  },
 ];
